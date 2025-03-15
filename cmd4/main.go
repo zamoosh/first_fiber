0c656e3bfbd1830675645c358c628c66455d5bc6
Mohammad Rahimi <moyi.pary@gmail.com>
2025-03-16 03:21:22 +0330

    could connect to mongo without problem

--- cmd4/main.go
+++ cmd4/main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"first_fiber"
-	"first_fiber/databases/mongo"
 	agencyAdmin "first_fiber/handlers/agency/admin"
 	clientAuth "first_fiber/handlers/client/auth"
-	"first_fiber/library/custom_log"
-
 	"github.com/charmbracelet/log"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 var (
@@ -29,9 +25,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("Could not load project confings. %s", err.Error())
 	}
-
-	c := mongo.CountDocuments(mongo.MCDColl(mongo.ActiveGpsLog), bson.D{})
-	custom_log.L.Warnf("cound: %d", c)
 
 	app := fiber.New(config)
 	app.Use(logger.New(logger.Config{TimeZone: "Asia/Tehran"}))
